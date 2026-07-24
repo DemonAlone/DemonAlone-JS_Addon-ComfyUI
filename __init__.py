2d3156eccb79bade8254d3b8c8dea96b6e287758
DemonAlone <25270760+DemonAlone@users.noreply.github.com>
2026-07-25 00:14:54 +0300

    v1.2.0
    fixed _init_.py

--- __init__.py
+++ __init__.py
@@ -1,17 +1,20 @@
 from .videomaker import VideoMakerNode
 from .load_video import LoadVideoNode
 from .play_sound import DA_PlaySound
+from .PresetFloat import PresetFloatNode
 
 NODE_CLASS_MAPPINGS = {
-    "VideoMakerNode": VideoMakerNode,
-	"LoadVideoNode": LoadVideoNode,
     "DA_PlaySound": DA_PlaySound,
+    "LoadVideoNode": LoadVideoNode,
+    "PresetFloatNode": PresetFloatNode,
+    "VideoMakerNode": VideoMakerNode,   
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "VideoMakerNode": "Video Maker (Beta)",
-	"LoadVideoNode": "Load Video (Beta)",
     "DA_PlaySound": "DA_PlaySound (Beta)",
+    "LoadVideoNode": "LoadVideoNode (Beta)",
+    "PresetFloatNode": "PresetFloatNode (Beta)",
+    "VideoMakerNode": "VideoMakerNode (Beta)", 
 }
 
 WEB_DIRECTORY = "./web"
